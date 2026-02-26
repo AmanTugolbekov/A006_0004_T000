92e49693bb66f5475ff0946bb95a76fea61b7ad9
AmanTugolbekov <161336104+AmanTugolbekov@users.noreply.github.com>
2026-02-26 13:02:40 +0600

    Update Task_1.java

--- D021/Task_1.java
+++ D021/Task_1.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] scores = new int[n];
-        int sum = 0;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            scores[i] = sc.nextInt();
-            sum += scores[i];
-            min = Math.min(min, scores[i]);
+        System.out.println("Print amount of integers: ");
+        int amount = sc.nextInt();
+        int[] arr = new int[amount];
+        System.out.println("Type in the integers: ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
         }
-        double average = (double)(sum - min) / (n - 1);
-        System.out.println(average);
+        int lowest = arr[0];
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < lowest){
+                lowest = arr[i];
+            }
+            sum += arr[i];
+        }
+        double average = (double) (sum - lowest) / (arr.length - 1);
+        System.out.println("The average is: ");
+        System.out.printf("%.1f%n", average);
     }
 }
